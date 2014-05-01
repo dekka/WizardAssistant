a1b1be608c19b60bf16d6445370cbcddd14d91b3
John Clem <johnnyclem@gmail.com>
2014-05-01 15:06:46 -0700

    add .gitignore

---
+++ WizardAssistant/PlayerCelPanelView.h
@@ -0,0 +1,13 @@
+//
+//  PlayerCelPanelView.h
+//  WizardAssistant
+//
+//  Created by Reed Sweeney on 5/1/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerCelPanelView : UIView
+
+@end
---
+++ WizardAssistant/PlayerCelPanelView.m
@@ -0,0 +1,31 @@
+//
+//  PlayerCelPanelView.m
+//  WizardAssistant
+//
+//  Created by Reed Sweeney on 5/1/14.
+//  Copyright (c) 2014 Reed Sweeney. All rights reserved.
+//
+
+#import "PlayerCelPanelView.h"
+
+@implementation PlayerCelPanelView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
